da9a36628f3aecd01f3618f530fd94d8d3a86fe6
AlexKranov <alexkranov@mail.ru>
2022-01-12 23:04:09 +0300

    *

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const characters = [
-<<<<<<< HEAD
   { name: 'мечник', health: 10 },
   { name: 'маг', health: 100 },
   { name: 'маг', health: 0 },
@@ -7,12 +6,3 @@ const characters = [
 ];
 
 const alive = characters.filter((item) => item.health > 0);
-=======
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'маг', health: 0 },
-    { name: 'лучник', health: 0 },
-];
-
-const alive = characters.filter((item) => item.health > 0);
->>>>>>> 10a8197358339b6d6e11ce5e091b63227aabd5dd
